df13431bc2c1a6833c7f72af8c2884ad106abb82
Pychey <prakpycheyy@gmail.com>
2025-03-27 14:14:49 +0700

    Completed Exercise-1 (onChange scoreWidth & Color)

--- exercice 1/src/App.jsx
+++ exercice 1/src/App.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
+  const [scoreWidth,setScoreWidth] = useState('40%');
+  const [scoreColor,setScoreColor] = useState("rgb(255,0,0)");
+
+  function onChangeFunctions(e){
+    if(e.target.value >= 0 && e.target.value <= 10){
+      getScoreWidth(e);
+      getScoreColor(e);
+    }
+  }
+
+  function getScoreWidth(e) {
+    let width = (parseInt(e.target.value) * 10) + '%';
+    setScoreWidth(width);
+  }
+
+  function getScoreColor(e) {
+    let red = 255 - (e.target.value * 25.5);
+    let green = e.target.value * 25.5;
+    setScoreColor("rgb("+ parseInt(red) + "," + parseInt(green) + ",0)")
+  }
+
   const getScoreBarStyle = () => {
-    // 1- Compute width
-    const scoreWidth = `40%`;
-
-    // 2- Compute color (optional)
-    let scoreColor = `#f3bc47`;
-
-    // 3 - Return the style object
     return {
       width: scoreWidth,
       backgroundColor: scoreColor,
@@ -21,7 +35,7 @@ export default function App() {
         <h1>My Score in React</h1>
 
         <small>Enter a score (0 to 10): </small>
-        <input type="number" min="0" max="10"></input>
+        <input type="number" min="0" max="10" onChange={onChangeFunctions}></input>
 
         <div className="score-bar">
           <div className="score-bar-value" style={getScoreBarStyle()}></div>
